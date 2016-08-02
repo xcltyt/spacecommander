526aadbfb5800836279d5852b679ca3476423110
young.huang <young.huang@jfz.com>
2016-08-02 17:09:30 +0800

    mod

--- code-reset.sh
+++ code-reset.sh
@@ -11,8 +11,15 @@ for (( i = 0; i < num; i++ )); do
 
 if [ $i -eq 0 ]; then
 	echo -e "\033[33mformat-id:"${arr[i]}"\033[0m"
-else
-	echo -e ${arr[i]}
+elif [ $i -eq 1 ]; then
+	echo -e "time:"${arr[i]}
+# elif [ $i -eq 2 ]; then
+	# modcontent=${arr[i]}
+	# modarr=(${modcontent//^/ })  
+	# size=${#modarr[@]}   
+	# for (( j = 0; j < size; j++ )); do
+	# echo -e ${modarr[j]}
+	# done
 fi
 done 
  echo -e ""
