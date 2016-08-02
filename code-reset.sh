7fae728c8d5731bcdca33b3693bc80eb952e62e3
young.huang <young.huang@jfz.com>
2016-08-02 15:52:53 +0800

    mod

--- code-reset.sh
+++ code-reset.sh
@@ -1,6 +1,19 @@
+#!/usr/bin/env bash
 
 filepath=$(cd "$(dirname "$0")"; pwd)
 cat $filepath"/code-check.log" | while read myline
 do 
- echo "LINE:"$myline
+ echo -e ""
+arr=(${myline//&/ })  
+num=${#arr[@]}   
+
+for (( i = 0; i < num; i++ )); do
+
+if [ $i -eq 0 ]; then
+	echo -e "\033[33mformat-id:"${arr[i]}"\033[0m"
+else
+	echo -e ${arr[i]}
+fi
+done 
+ echo -e ""
 done
