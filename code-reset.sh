fb05b081f0cae28aaeb16d9788061192ebeb963d
young.huang <young.huang@jfz.com>
2016-08-09 18:45:30 +0800

    mod

--- code-reset.sh
+++ code-reset.sh
@@ -9,5 +9,6 @@ backup_filePath=$DIR_BACKUP"/codeCheckerCache/"$filePath
 if [ -f $filePath ]; then
 	if [  -f $backup_filePath  ]; then
          $(cp  "$backup_filePath" "$filePath") 
+		 echo -e "\033[32m>> reset '$filePath' success \033[0m\n"
 	fi
 fi
